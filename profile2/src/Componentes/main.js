8b135d058f9342c28369f6ea31411b2413c42917
sandyvmarques <aluno19997@ipt.pt>
2019-03-30 19:25:01 +0000

    criacao de outra rotas

--- profile2/src/Componentes/main.js
+++ profile2/src/Componentes/main.js
@@ -3,11 +3,13 @@
  */
 import React from "react";
 import PP from "./PP";
+import Projectos from "./Projectos";
 import { Switch, Route } from "react-router-dom";
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={PP} />
+    <Route path="/Projectos" component={Projectos} />
   </Switch>
 );
 export default Main;
