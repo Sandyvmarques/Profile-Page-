3e60227d47a283704f62de0ca5ca239e2e082917
sandyvmarques <aluno19997@ipt.pt>
2019-03-30 19:07:06 +0000

    Criacao de links

---
+++ profile2/src/Componentes/PP.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class PP extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Página de Perfil</h1>
+      </div>
+    );
+  }
+}
+export default PP;
